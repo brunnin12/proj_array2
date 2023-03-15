7f562a6cc48784ae141bcafcb96bd0ae253426f7
Brunno <brunnopereira090@gmail.com>
2023-03-15 10:28:59 -0300

    Programa finalizado

--- vetor.js
+++ vetor.js
@@ -8,8 +8,23 @@ function inserir(){
     res.innerHTML = valor_cidade
 }
 
+function insert_inicio(){
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.unshift(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+
+
+
 function excluir(){
     valor_cidade.pop()
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+
+function excluir_final(){
+    valor_cidade.shift()
     console.log(valor_cidade)
     res.innerHTML = valor_cidade
 }
