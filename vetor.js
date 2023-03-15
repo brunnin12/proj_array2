1efd7ce5d1ec592a6eb54bd5ebd439d40d2cf06f
Brunno <brunnopereira090@gmail.com>
2023-03-15 09:43:24 -0300

    JS finalizado com inserir e excluir

--- vetor.js
+++ vetor.js
@@ -1,12 +1,15 @@
 let valor_cidade = []
+let res = document.getElementById('res')
 
 function inserir(){
     let cidade = document.getElementById('cidade').value
     valor_cidade.push(cidade)
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade
 }
 
 function excluir(){
-    valor_cidade.pop(cidade)
+    valor_cidade.pop()
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade
 }
